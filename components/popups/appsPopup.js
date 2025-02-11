a4103c4b5b496f154e62dd78a559ea2b078e93c8
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2025-02-10 18:45:19 -0800

    Clean up styling

--- components/popups/appsPopup.js
+++ components/popups/appsPopup.js
@@ -29,40 +29,34 @@ export default function AppsPopup() {
             <line x1="6" y1="6" x2="18" y2="18"></line>
           </svg>
         </button>
-        <div className={styles.googleApps}>
-          <a href="https://www.google.com"><img src={"newTabDefault.svg"} alt={"New Tab Default button"}
-                                                width="64" height="64"/> </a>
-          <a href="https://drive.google.com/drive/u/0/home"><img src={"drive.svg"}
-                                                                 alt={"Google Drive button"}
-                                                                 width="64" height="64"/> </a>
-          <a href="https://mail.google.com/mail/u/0/#inbox"><img src={"mail.svg"} alt={"Mail button"}
-                                                                 width="64" height="64"/> </a>
-          <a href="https://www.youtube.com/?authuser=0"><img src={"youtube.svg"} alt={"YouTube button"}
-                                                             width="64" height="64"/> </a>
-        </div>
-        <div className={styles.googleApps}>
-          <a href="https://calendar.google.com/calendar/u/0/r"><img src={"calendar.svg"}
-                                                                    alt={"Calendar button"}
-                                                                    width="64" height="64"/> </a>
-
-          <a href="https://steamdb.info/">
-            <div className={styles.steamdb}><img
-              src={"steamdb.svg"}
-              alt={"SteamDB button"}
-              width="64" height="64"/>
-            </div>
+        <div className={styles.apps}>
+          <a href="https://www.google.com">
+            <img src={"newTabDefault.svg"} alt={"New Tab Default button"} width="64" height="64"/>
           </a>
-          <a href="https://www.humblebundle.com/"><img src={"humble.svg"}
-                                                       alt={"Humble Bundle button"}
-                                                       width="64" height="64"/> </a>
-          <a href="https://read.amazon.com/kindle-library"><img src={"book.svg"}
-                                                                alt={"Kindle button"}
-                                                                width="64" height="64"/> </a>
-        </div>
-        <div className={styles.googleApps}>
-          <a href="https://libbyapp.com/shelf"><img src={"libby.svg"}
-                                                    alt={"Libby button"}
-                                                    width="64" height="64"/> </a>
+          <a href="https://drive.google.com/drive/u/0/home">
+            <img src={"drive.svg"} alt={"Google Drive button"} width="64" height="64"/>
+          </a>
+          <a href="https://mail.google.com/mail/u/0/#inbox">
+            <img src={"mail.svg"} alt={"Mail button"} width="64" height="64"/>
+          </a>
+          <a href="https://www.youtube.com/?authuser=0">
+            <img src={"youtube.svg"} alt={"YouTube button"} width="64" height="64"/>
+          </a>
+          <a href="https://calendar.google.com/calendar/u/0/r">
+            <img src={"calendar.svg"} alt={"Calendar button"} width="64" height="64"/>
+          </a>
+          <a href="https://steamdb.info/">
+              <img src={"steamdb.svg"} alt={"SteamDB button"} width="64" height="64"/>
+          </a>
+          <a href="https://www.humblebundle.com/">
+            <img src={"humble.svg"} alt={"Humble Bundle button"} width="64" height="64"/>
+          </a>
+          <a href="https://read.amazon.com/kindle-library">
+            <img src={"book.svg"} alt={"Kindle button"} width="64" height="64"/>
+          </a>
+          <a href="https://libbyapp.com/shelf">
+            <img src={"libby.svg"} alt={"Libby button"} width="64" height="64"/>
+          </a>
         </div>
       </div>
     </div>}
--- components/popups/gamesPopup.js
+++ components/popups/gamesPopup.js
@@ -12,47 +12,43 @@ export default function GamesPopup() {
   const launchGame = (game) => {
     game(true)
   }
-  return (
-    <>
-      <img
-        onClick={() => setPopupOpen(true)}
-        src={"games.png"}
-        className={styles.popupTrigger}
-        alt="Games Button"
-      />
-      {popupOpen && <>
-        <div className={styles.fullscreen}>
-          <div className={styles.popup}>
-            <button onClick={() => setPopupOpen(false)} className={styles.close}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
-              </svg>
-            </button>
-            <h1> Games </h1>
-            <button className={styles.button} onClick={() => launchGame(setMadLibsPopup)}>Mad Libs</button>
-            <button className={styles.button} onClick={() => launchGame(setSpaghettiYetiPopup)}>Spaghetti Yeti</button>
-          </div>
+  return (<>
+    <img
+      onClick={() => setPopupOpen(true)}
+      src={"games.png"}
+      className={styles.popupTrigger}
+      alt="Games Button"
+    />
+    {popupOpen && <>
+      <div className={styles.fullscreen}>
+        <div className={styles.popup}>
+          <button onClick={() => setPopupOpen(false)} className={styles.close}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </svg>
+          </button>
+          <h3>Games</h3>
+          <button className={styles.button} onClick={() => launchGame(setMadLibsPopup)}>Mad Libs</button>
+          <button className={styles.button} onClick={() => launchGame(setSpaghettiYetiPopup)}>Spaghetti Yeti</button>
         </div>
-        {madLibsPopup && (
-          <MadLibs
-            closeMethod={() => setMadLibsPopup(false)}
-          />)}
-        {spaghettiYetiPopup && (
-          <SpaghettiYeti
-            closeMethod={() => setSpaghettiYetiPopup(false)}
-          />)}
-      </>}
-    </>
-  );
+      </div>
+      {madLibsPopup && (<MadLibs
+        closeMethod={() => setMadLibsPopup(false)}
+      />)}
+      {spaghettiYetiPopup && (<SpaghettiYeti
+        closeMethod={() => setSpaghettiYetiPopup(false)}
+      />)}
+    </>}
+  </>);
 }
